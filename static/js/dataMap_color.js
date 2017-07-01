ae511211a5c8295cde363d454c420a61fbdc1aca
catdintran <trancatdin86@yahoo.com>
2017-07-01 03:01:11 -0400

    Update dataMap_color.js

--- static/js/dataMap_color.js
+++ static/js/dataMap_color.js
@@ -1,6 +1,159 @@
-function fis2015(map) {
-    map.updateChoropleth({USA: 'green' , CAN: 'red'}, {reset: true});   
+function fis2017(map) {
+    /*
+    {'BEL': 'Tax policy Adjustment',
+     'GRC': 'Fiscal Risk',
+     'ISR': 'Fiscal Consolidation',
+     'SVK': 'Fiscal Consolidation',
+     'TUR': 'Fiscal Risk'}
+     
+     Expansionary Fiscal Pilocy         #7E57C2
+     Fiscal Consolidation               #F39C12
+     Fiscal Risk  :                     #E74C3C
+     Medium Term Fiscal Adjustment      #2980B9
+     Tax policy Adjustment  :           #45B39D
+    */
+    map.updateChoropleth({BEL: '#45B39D' , GRC: '#E74C3C', ISR: '#F39C12', SVK: '#F39C12', TUR : '#E74C3C'}, {reset: true});   
 
+}
+function fis2016(map) {
+     /*
+    {'AGO': 'Fiscal Consolidation',
+ 'ALB': 'Tax policy Adjustment',
+ 'ARG': 'Fiscal Consolidation',
+ 'AUT': 'Fiscal Consolidation',
+ 'AZE': 'Expansionary Fiscal Pilocy',
+ 'BFA': 'Expansionary Fiscal Pilocy',
+ 'BLR': 'Fiscal Consolidation',
+ 'BOL': 'Fiscal Consolidation',
+ 'BRA': 'Fiscal Consolidation',
+ 'BTN': 'Medium Term Fiscal Adjustment',
+ 'CAF': 'Expansionary Fiscal Pilocy',
+ 'CAN': 'Tax policy Adjustment',
+ 'CPV': 'Fiscal Risk',
+ 'CRI': 'Fiscal Consolidation',
+ 'CZE': 'Fiscal Consolidation',
+ 'DEU': 'Expansionary Fiscal Pilocy',
+ 'DJI': 'Fiscal Risk',
+ 'DMA': 'Fiscal Consolidation',
+ 'DZA': 'Expansionary Fiscal Pilocy',
+ 'EST': 'Fiscal Consolidation',
+ 'ETH': 'Fiscal Risk',
+ 'FIN': 'Tax policy Adjustment',
+ 'FRA': 'Fiscal Consolidation',
+ 'GBR': 'Tax policy Adjustment',
+ 'GNQ': 'Expansionary Fiscal Pilocy',
+ 'GRD': 'Fiscal Consolidation',
+ 'GUY': 'Expansionary Fiscal Pilocy',
+ 'HND': 'Expansionary Fiscal Pilocy',
+ 'HRV': 'Fiscal Consolidation',
+ 'HUN': 'Fiscal Consolidation',
+ 'KIR': 'Expansionary Fiscal Pilocy',
+ 'LKA': 'Tax policy Adjustment',
+ 'LUX': 'Fiscal Consolidation',
+ 'MEX': 'Fiscal Consolidation',
+ 'MHL': 'Tax policy Adjustment',
+ 'MLT': 'Fiscal Consolidation',
+ 'NAM': 'Fiscal Consolidation',
+ 'NER': 'Fiscal Consolidation',
+ 'NOR': 'Medium Term Fiscal Adjustment',
+ 'PAN': 'Medium Term Fiscal Adjustment',
+ 'PER': 'Expansionary Fiscal Pilocy',
+ 'PNG': 'Tax policy Adjustment',
+ 'POL': 'Fiscal Consolidation',
+ 'PRY': 'Expansionary Fiscal Pilocy',
+ 'ROM': 'Fiscal Consolidation',
+ 'SDN': 'Tax policy Adjustment',
+ 'SLV': 'Tax policy Adjustment',
+ 'SMR': 'Fiscal Consolidation',
+ 'SSD': 'Fiscal Consolidation',
+ 'SVN': 'Fiscal Consolidation',
+ 'SWE': 'Fiscal Consolidation',
+ 'TUR': 'Medium Term Fiscal Adjustment',
+ 'TZA': 'Expansionary Fiscal Pilocy',
+ 'URY': 'Tax policy Adjustment',
+ 'VNM': 'Tax policy Adjustment',
+ 'VUT': 'Fiscal Consolidation',
+ 'ZAF': 'Fiscal Consolidation',
+ 'ZWE': 'Fiscal Risk'}
+     
+     Expansionary Fiscal Pilocy         #7E57C2
+     Fiscal Consolidation               #F39C12
+     Fiscal Risk  :                     #E74C3C
+     Medium Term Fiscal Adjustment      #2980B9
+     Tax policy Adjustment  :           #45B39D
+    */
+    map.updateChoropleth({DZA: '#7E57C2', AGO: '#F39C12',GBR: '#45B39D', GNQ: '#7E57C2',HND: '#7E57C2',DMA: '#F39C12',LUX: '#F39C12',SWE: '#F39C12',FRA: '#F39C12',PER: '#7E57C2',NAM: '#F39C12',VUT: '#F39C12',BRA: '#F39C12',BOL: '#F39C12',CAN: '#45B39D',GUY: '#7E57C2',CRI: '#F39C12',NER: '#F39C12',ETH: '#E74C3C',CPV: '#E74C3C',ROM: '#F39C12',HUN: '#F39C12',CAF: '#7E57C2',TZA: '#7E57C2',POL: '#F39C12',PAN: '#2980B9',PRY: '#7E57C2',DEU: '#7E57C2',BFA: '#7E57C2',EST: '#F39C12',SLV: '#45B39D',HRV: '#F39C12',AZE: '#7E57C2',LKA: '#45B39D',GRD: '#F39C12',VNM: '#45B39D',SMR: '#F39C12',AUT: '#F39C12',MLT: '#F39C12',DJI: '#E74C3C',BLR: '#F39C12',FIN: '#45B39D',URY: '#45B39D',ZWE: '#E74C3C',MHL: '#45B39D',KIR: '#7E57C2',TUR: '#2980B9',ALB: '#45B39D',ZAF: '#F39C12',NOR: '#2980B9',ARG: '#F39C12',SVN: '#F39C12',SSD: '#F39C12',SDN: '#45B39D',BTN: '#2980B9',MEX: '#F39C12',PNG: '#45B39D',CZE: '#F39C12'}, {reset: true});   
+  
+
+}
+function fis2015(map) {
+    /*
+    {'AGO': 'Fiscal Consolidation',
+ 'ALB': 'Tax policy Adjustment',
+ 'ARG': 'Fiscal Consolidation',
+ 'AUT': 'Fiscal Consolidation',
+ 'AZE': 'Expansionary Fiscal Pilocy',
+ 'BFA': 'Expansionary Fiscal Pilocy',
+ 'BLR': 'Fiscal Consolidation',
+ 'BOL': 'Fiscal Consolidation',
+ 'BRA': 'Fiscal Consolidation',
+ 'BTN': 'Medium Term Fiscal Adjustment',
+ 'CAF': 'Expansionary Fiscal Pilocy',
+ 'CAN': 'Tax policy Adjustment',
+ 'CPV': 'Fiscal Risk',
+ 'CRI': 'Fiscal Consolidation',
+ 'CZE': 'Fiscal Consolidation',
+ 'DEU': 'Expansionary Fiscal Pilocy',
+ 'DJI': 'Fiscal Risk',
+ 'DMA': 'Fiscal Consolidation',
+ 'DZA': 'Expansionary Fiscal Pilocy',
+ 'EST': 'Fiscal Consolidation',
+ 'ETH': 'Fiscal Risk',
+ 'FIN': 'Tax policy Adjustment',
+ 'FRA': 'Fiscal Consolidation',
+ 'GBR': 'Tax policy Adjustment',
+ 'GNQ': 'Expansionary Fiscal Pilocy',
+ 'GRD': 'Fiscal Consolidation',
+ 'GUY': 'Expansionary Fiscal Pilocy',
+ 'HND': 'Expansionary Fiscal Pilocy',
+ 'HRV': 'Fiscal Consolidation',
+ 'HUN': 'Fiscal Consolidation',
+ 'KIR': 'Expansionary Fiscal Pilocy',
+ 'LKA': 'Tax policy Adjustment',
+ 'LUX': 'Fiscal Consolidation',
+ 'MEX': 'Fiscal Consolidation',
+ 'MHL': 'Tax policy Adjustment',
+ 'MLT': 'Fiscal Consolidation',
+ 'NAM': 'Fiscal Consolidation',
+ 'NER': 'Fiscal Consolidation',
+ 'NOR': 'Medium Term Fiscal Adjustment',
+ 'PAN': 'Medium Term Fiscal Adjustment',
+ 'PER': 'Expansionary Fiscal Pilocy',
+ 'PNG': 'Tax policy Adjustment',
+ 'POL': 'Fiscal Consolidation',
+ 'PRY': 'Expansionary Fiscal Pilocy',
+ 'ROM': 'Fiscal Consolidation',
+ 'SDN': 'Tax policy Adjustment',
+ 'SLV': 'Tax policy Adjustment',
+ 'SMR': 'Fiscal Consolidation',
+ 'SSD': 'Fiscal Consolidation',
+ 'SVN': 'Fiscal Consolidation',
+ 'SWE': 'Fiscal Consolidation',
+ 'TUR': 'Medium Term Fiscal Adjustment',
+ 'TZA': 'Expansionary Fiscal Pilocy',
+ 'URY': 'Tax policy Adjustment',
+ 'VNM': 'Tax policy Adjustment',
+ 'VUT': 'Fiscal Consolidation',
+ 'ZAF': 'Fiscal Consolidation',
+ 'ZWE': 'Fiscal Risk'}
+     
+     Expansionary Fiscal Pilocy         #7E57C2
+     Fiscal Consolidation               #F39C12
+     Fiscal Risk  :                     #E74C3C
+     Medium Term Fiscal Adjustment      #2980B9
+     Tax policy Adjustment  :           #45B39D
+    */
+    map.updateChoropleth({DZA: '#7E57C2', AGO: '#F39C12',GBR: '#45B39D', GNQ: '#7E57C2',HND: '#7E57C2',DMA: '#F39C12',LUX: '#F39C12',SWE: '#F39C12',FRA: '#F39C12',PER: '#7E57C2',NAM: '#F39C12',VUT: '#F39C12',BRA: '#F39C12',BOL: '#F39C12',CAN: '#45B39D',GUY: '#7E57C2',CRI: '#F39C12',NER: '#F39C12',ETH: '#E74C3C',CPV: '#E74C3C',ROM: '#F39C12',HUN: '#F39C12',CAF: '#7E57C2',TZA: '#7E57C2',POL: '#F39C12',PAN: '#2980B9',PRY: '#7E57C2',DEU: '#7E57C2',BFA: '#7E57C2',EST: '#F39C12',SLV: '#45B39D',HRV: '#F39C12',AZE: '#7E57C2',LKA: '#45B39D',GRD: '#F39C12',VNM: '#45B39D',SMR: '#F39C12',AUT: '#F39C12',MLT: '#F39C12',DJI: '#E74C3C',BLR: '#F39C12',FIN: '#45B39D',URY: '#45B39D',ZWE: '#E74C3C',MHL: '#45B39D',KIR: '#7E57C2',TUR: '#2980B9',ALB: '#45B39D',ZAF: '#F39C12',NOR: '#2980B9',ARG: '#F39C12',SVN: '#F39C12',SSD: '#F39C12',SDN: '#45B39D',BTN: '#2980B9',MEX: '#F39C12',PNG: '#45B39D',CZE: '#F39C12'}, {reset: true});   
 }
 
           
